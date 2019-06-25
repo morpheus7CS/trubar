557b47864dcadcb8e0fb456b8bfa3dfde7c7d2f1
Gal Jakič <gal.jakic@gmail.com>
2019-06-25 23:22:06 +0200

    Create TrubarPostType.php

---
+++ Trubar/Admin/Models/TrubarPostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Trubar\Admin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TrubarPostType extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'trubar_post_types';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    public function posts()
+    {
+        return $this->belongsToMany(TrubarPost::class);
+    }
+}
