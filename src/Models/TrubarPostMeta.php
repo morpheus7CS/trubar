b73eb67497ecf0ab972847ee33a851d2c2ea9b60
Gal Jakic (We Wow Web) <gal.jakic@gmail.com>
2019-06-30 01:24:16 +0200

    Add TrubarPostMeta

--- src/Models/TrubarPostMeta.php
+++ src/Models/TrubarPostMeta.php
@@ -3,8 +3,19 @@
 namespace Wewowweb\Trubar\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wewowweb\Trubar\Models\TrubarPost;
 
 class TrubarPostMeta extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'meta',
+        'post_id'
+    ];
+
+    public function post(){
+        return $this->belongsTo(TrubarPost::class);
+    }
 }
