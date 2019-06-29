2847b44698d4e8715b5cb659641ccb5b3c4fa492
Gal Jakic (We Wow Web) <gal.jakic@gmail.com>
2019-06-30 01:13:02 +0200

    Add TrubarPostType

--- src/Models/TrubarPostType.php
+++ src/Models/TrubarPostType.php
@@ -3,8 +3,17 @@
 namespace Wewowweb\Trubar\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wewowweb\Trubar\Models\TrubarPost;
 
 class TrubarPostType extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['slug'];
+
+    public function post()
+    {
+        return $this->belongsTo(TrubarPost::class);
+    }
 }
